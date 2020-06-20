1ecd4b189883fb17adc234fbf8bd53837c7b47cd
Mikhail Mishin <excentro@gmail.com>
2020-06-20 16:56:26 +0300

    Новый файловый менеджер

--- client/src/main/java/com/excentro/netstorage/gui/Controller.java
+++ client/src/main/java/com/excentro/netstorage/gui/Controller.java
@@ -1,4 +1,57 @@
 package com.excentro.netstorage.gui;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Controller {
+  @FXML
+  VBox localFiles;
+
+  @FXML
+  VBox remoteFiles;
+
+  public void cmdExit() {
+    Platform.exit();
+  }
+
+  public void copyBtnAction(final ActionEvent actionEvent) {
+    PanelController localPC = (PanelController) localFiles.getProperties()
+                                                          .get("ctrl");
+    PanelController remotePC = (PanelController) remoteFiles.getProperties()
+                                                            .get("ctrl");
+
+    if (localPC.getSelectedFileName() == null && remotePC.getSelectedFileName() == null) {
+      Alert alert = new Alert(Alert.AlertType.ERROR, "No files chosen", ButtonType.OK);
+      alert.showAndWait();
+    }
+    PanelController srcPC = null, dstPC = null;
+    if (localPC.getSelectedFileName() != null) {
+      srcPC = localPC;
+      dstPC = remotePC;
+    }
+    if (remotePC.getSelectedFileName() != null) {
+      srcPC = remotePC;
+      dstPC = localPC;
+    }
+    Path srcPath = Paths.get(srcPC.getCurrentPath(), srcPC.getSelectedFileName());
+    Path dstPath = Paths.get(dstPC.getCurrentPath())
+                        .resolve(srcPath.getFileName()
+                                        .toString());
+    try {
+      Files.copy(srcPath, dstPath);
+      dstPC.updateFiles(Paths.get(dstPC.getCurrentPath()));
+    } catch (IOException e) {
+      Alert alert = new Alert(Alert.AlertType.ERROR, "File already exists", ButtonType.OK);
+      alert.showAndWait();
+    }
+  }
 }
--- client/src/main/java/com/excentro/netstorage/gui/RunGUI.java
+++ client/src/main/java/com/excentro/netstorage/gui/RunGUI.java
@@ -16,7 +16,7 @@ public class RunGUI extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("/main_screen.fxml"));
     primaryStage.setTitle("Network Storage Client");
-    primaryStage.setScene(new Scene(root, 640, 400));
+    primaryStage.setScene(new Scene(root, 1200, 600));
     primaryStage.show();
   }
 }
