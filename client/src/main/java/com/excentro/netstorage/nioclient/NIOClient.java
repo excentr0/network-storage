4b5de33758415687f47ae0d1888822d409349ce9
Mikhail Mishin <excentro@gmail.com>
2020-06-24 13:27:49 +0300

    файловый сервер и клиент на NIO

---
+++ client/src/main/java/com/excentro/netstorage/nioclient/NIOClient.java
@@ -0,0 +1,37 @@
+package com.excentro.netstorage.nioclient;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+/** Скачивает файл с сервера на НИО */
+public class NIOClient {
+
+  public static void main(String[] args) throws IOException {
+    String fileToWrite = "D:/tmp/file.txt";
+
+    try (SocketChannel client = SocketChannel.open()) {
+      // Открываем соединение на localhost порт 8888
+      SocketAddress socketAddr = new InetSocketAddress(8888);
+      client.connect(socketAddr);
+      // Открываем файл на запись
+      try (RandomAccessFile writer = new RandomAccessFile(fileToWrite, "rw")) {
+        FileChannel fileChannel = writer.getChannel();
+        // буфер чтения
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+        // читаем в буфер данные
+        while (client.read(buffer) > 0) {
+          buffer.flip();
+          // пишем в файл
+          fileChannel.write(buffer);
+          buffer.clear();
+        }
+      }
+      System.out.println("File downloaded");
+    }
+  }
+}
--- server/src/main/java/com/excentro/netstorage/server/nioserver/NIOServer.java
+++ server/src/main/java/com/excentro/netstorage/server/nioserver/NIOServer.java
@@ -3,94 +3,37 @@ package com.excentro.netstorage.server.nioserver;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
+import java.nio.channels.FileChannel;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Iterator;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class NIOServer implements Runnable {
-  private static int clientCount = 0;
-  private final ByteBuffer buffer = ByteBuffer.allocate(256);
-  private final ServerSocketChannel serverSocketChannel;
-  private final Selector selector;
-
-  public NIOServer() throws IOException {
-    serverSocketChannel = ServerSocketChannel.open();
-    serverSocketChannel.socket().bind(new InetSocketAddress(8189));
-    serverSocketChannel.configureBlocking(false);
-    selector = Selector.open();
-    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-  }
+/** Запускает NIO сервер на порту 8888 и ждет подключения, после чего отправляет файл */
+public class NIOServer {
 
   public static void main(String[] args) throws IOException {
-    new Thread(new NIOServer()).start();
-  }
+    Path path = Paths.get("./", "download", "file.txt");
 
-  public void run() {
-    try {
-      System.out.println("Server started on port 8189");
-      Iterator<SelectionKey> iterator;
-      SelectionKey key;
-      while (serverSocketChannel.isOpen()) {
-        int eventsCount = selector.select();
-        System.out.println("Selected " + eventsCount + " events.");
-        iterator = selector.selectedKeys().iterator();
-        while (iterator.hasNext()) {
-          key = iterator.next();
-          iterator.remove();
-          if (key.isAcceptable()) {
-            handleAccess(key);
-          }
-          if (key.isReadable()) {
-            handleRead(key);
-          }
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    SocketChannel server;
+    // запускаем сервер на порту 8888
+    try (ServerSocketChannel serverSocket = ServerSocketChannel.open()) {
+      serverSocket.socket().bind(new InetSocketAddress(8888));
+      // принимаем соединения
+      server = serverSocket.accept();
     }
-  }
-
-  private void handleAccess(final SelectionKey key) throws IOException {
-    SocketChannel channel = ((ServerSocketChannel) key.channel()).accept();
-    clientCount++;
-    String userName = "user#" + clientCount;
-    channel.configureBlocking(false);
-    channel.register(selector, SelectionKey.OP_READ, userName);
-    channel.write(ByteBuffer.wrap(("Hello " + userName + "!").getBytes()));
-    System.out.println("Client " + userName + " connected from ip: " + channel.getLocalAddress());
-  }
-
-  private void handleRead(final SelectionKey key) throws IOException {
-    SocketChannel channel = (SocketChannel) key.channel();
-    StringBuilder message = new StringBuilder();
-    int read = 0;
-    buffer.rewind();
-    while ((read = channel.read(buffer)) > 0) {
-      buffer.flip();
-      byte[] bytes = new byte[buffer.limit()];
-      buffer.get(bytes);
-      message.append(new String(bytes));
-      buffer.rewind();
-    }
-    if (read < 0) {
-      System.out.println(key.attachment() + ": leave!");
-      for (SelectionKey send : key.selector().keys()) {
-        if (send.channel() instanceof SocketChannel && send.isReadable()) {
-          ((SocketChannel) send.channel())
-              .write(ByteBuffer.wrap((key.attachment() + ": leave!").getBytes()));
-        }
-      }
-      channel.close();
-    } else {
-      System.out.println(key.attachment() + ": " + message);
-      String msg = key.attachment() + ": " + message;
-      for (SelectionKey send : key.selector().keys()) {
-        if (send.channel() instanceof SocketChannel && send.isReadable()) {
-          ((SocketChannel) send.channel()).write(ByteBuffer.wrap(msg.getBytes()));
-        }
+    System.out.println("Got connection from:  " + server.getRemoteAddress());
+    // открываем файл
+    try (FileChannel fileChannel = FileChannel.open(path)) {
+      ByteBuffer buffer = ByteBuffer.allocate(256);
+      // читаем файл в буфер
+      while (fileChannel.read(buffer) > 0) {
+        buffer.flip();
+        // отправляем клиенту данные буфера
+        server.write(buffer);
+        buffer.clear();
       }
     }
+    server.close();
   }
 }
